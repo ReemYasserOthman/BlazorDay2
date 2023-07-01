1daf0c891b97c3956b103cfd0f372b703add5baa
ReemAtman <reematman15@gmail.com>
2023-07-01 14:49:29 +0300

    my first commit

---
+++ BlazorDay2/StateManagement/Employee/ChangeEmployeeState.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorDay2.StateManagement.Employee
+{
+    public class ChangeEmployeeState : IChangeEmployeeState
+    {
+        Action<string>? subscribers;
+        public string? name { get; private set; }
+
+        public void ChangeState(string state)
+        {
+            name = state;
+            if (subscribers != null)
+            {
+                subscribers.Invoke(name);
+            }
+        }
+
+        public void Subscribe(Action<string> subscriber)
+        {
+            subscribers += subscriber;
+        }
+
+        public void Unsubscribe(Action<string> subscriber)
+        {
+            subscribers -= subscriber;
+        }
+    }
+}
---
+++ BlazorDay2/StateManagement/Employee/IChangeEmployeeState.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorDay2.StateManagement.Employee
+{
+    public interface IChangeEmployeeState
+    {
+        string? name { get; }
+        void ChangeState(string state);
+        void Subscribe(Action<string> subscriber);
+        void Unsubscribe(Action<string> subscriber);
+    }
+}
